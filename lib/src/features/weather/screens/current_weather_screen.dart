f2eb2021e3896ac6e1bb82bae22bd51ceba11ebd
Martin Geidobler <martingeidobler@outlook.com>
2024-02-16 20:47:12 +0100

    MGE: added some files

---
+++ lib/src/features/weather/screens/current_weather_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CurrentWeatherScreen extends StatefulWidget {
+  const CurrentWeatherScreen({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _CurrentWeatherScreenState();
+}
+
+class _CurrentWeatherScreenState extends State<CurrentWeatherScreen> {
+  @override
+  Widget build(BuildContext context) {}
+}
